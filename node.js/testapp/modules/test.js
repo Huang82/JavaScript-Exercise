9c0150dde5f4e18bf4189f592ef46582b7ef30d7
Huang82 <72134109+Huang82@users.noreply.github.com>
2021-02-24 14:11:28 +0800

    update 2021/2/24

---
+++ node.js/testapp/modules/test.js
@@ -0,0 +1,13 @@
+
+function addition(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {
+    multi:multiply,
+    add:addition
+};
---
+++ node.js/testapp/routes/index.js
@@ -0,0 +1,11 @@
+const { text } = require('express');
+var express = require('express');
+var test = require('../modules/test');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: test.multi(5, 6) });
+});
+
+module.exports = router;
